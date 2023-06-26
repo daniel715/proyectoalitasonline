533339bd1b423857693129083c0b4fcd95c940b8
Daniel Montellanos <daniel.montellanos.contractor@bbva.com>
2023-06-26 17:20:37 -0500

    initial commit

---
+++ src/main/java/com/daniel/proyectoalitasonline/ProyectoalitasonlineApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.proyectoalitasonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoalitasonlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoalitasonlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daniel/proyectoalitasonline/ProyectoalitasonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.proyectoalitasonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoalitasonlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
